969c945d627dcf101021fd939112d8e449642de2
ArtKruzhkov <112978976+ArtKruzhkov@users.noreply.github.com>
2024-06-11 16:45:38 +0300

    FinalTaskReact

---
+++ FinalTask/final_task/src/components/productsData.js
@@ -0,0 +1,125 @@
+// Для Имитации бэкенда: массив данных о товарах
+const products = [
+  {
+    id: 1,
+    title: "ELLERY X M'O CAPSULE",
+    description:
+      "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    price: 52.0,
+    size: "M",
+    color: "Red",
+    imageUrl: "img/prod1.jpg",
+  },
+  {
+    id: 2,
+    title: "ELEGANT BLACK SUIT DRESS",
+    description:
+      "A sophisticated black suit dress designed for the modern woman. Perfect for formal events, this outfit combines a sleek dress with a tailored blazer.",
+    price: 120.0,
+    size: "S",
+    color: "Black",
+    imageUrl: "img/prod2.jpg",
+  },
+  {
+    id: 3,
+    title: "URBAN CHIC JACKET",
+    description:
+      "Elevate your street style with this chic jacket, perfect for layering during cooler seasons.",
+    price: 70.0,
+    size: "XS",
+    color: "Black",
+    imageUrl: "img/prod3.png",
+  },
+  {
+    id: 4,
+    title: "BRIGHT YELLOW PANTS",
+    description:
+      "These vibrant yellow pants are a bold addition to any man's wardrobe. Made from high-quality fabric, they offer both comfort and style.",
+    price: 60.0,
+    size: "L",
+    color: "Yellow",
+    imageUrl: "img/prod4.png",
+  },
+  {
+    id: 5,
+    title: "CHIC BLUE BLAZER",
+    description:
+      "Elevate your style with this chic blue blazer, perfect for adding a touch of sophistication to any outfit. Made from premium fabric.",
+    price: 75.0,
+    size: "L",
+    color: "Blue",
+    imageUrl: "img/prod5.png",
+  },
+  {
+    id: 6,
+    title: "OLIVE GREEN BLOUSE",
+    description:
+      "Add a touch of elegance to your wardrobe with this stylish olive green blouse. Featuring a flattering silhouette and intricate details.",
+    price: 65.0,
+    size: "M",
+    color: "Olive",
+    imageUrl: "img/prod6.png",
+  },
+  {
+    id: 7,
+    title: "CLASSIC WHITE SHIRT",
+    description:
+      "A versatile staple, this classic white shirt is perfect for both professional and casual looks.",
+    price: 30.0,
+    size: "S",
+    color: "White",
+    imageUrl: "img/prod7.png",
+  },
+  {
+    id: 8,
+    title: "STYLISH BLUE CAP",
+    description:
+      "Complete your casual look with this stylish blue cap. Made from high-quality materials.",
+    price: 25.0,
+    size: "M",
+    color: "Blue",
+    imageUrl: "img/prod8.png",
+  },
+  {
+    id: 9,
+    title: "FORMAL WHITE DRESS SHIRT",
+    description:
+      "Complete your formal look with this crisp white dress shirt. Tailored for a sleek fit, this shirt exudes sophistication and professionalism.",
+    price: 55.0,
+    size: "M",
+    color: "White",
+    imageUrl: "img/prod9.png",
+  },
+  {
+    id: 10,
+    title: "CLASSIC BLACK LEATHER JACKET",
+    description:
+      "Make a statement with this classic black leather jacket. Crafted from premium leather, this jacket offers both style and durability.",
+    price: 150.0,
+    size: "L",
+    color: "Black",
+    imageUrl: "../img/prod10.png",
+  },
+  {
+    id: 11,
+    title: "OLIVE SHORT SLEEVE SHIRT",
+    description:
+      "Stay cool and stylish with this olive short sleeve shirt for men. Made from breathable fabric, this shirt is perfect for everyday wear.",
+    price: 30.0,
+    size: "S",
+    color: "Olive",
+    imageUrl: "img/prod11.png",
+  },
+  {
+    id: 12,
+    title: "RED BUTTON-UP JACKET",
+    description:
+      "Add a pop of color to your wardrobe with this classic red button-up jacket. Made from durable fabric.",
+    price: 75.0,
+    size: "XS",
+    color: "Red",
+    imageUrl: "img/prod12.png",
+  }
+];
+
+export default products;
